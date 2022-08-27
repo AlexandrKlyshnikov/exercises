28215a0a474114b177bc6acc9aa4b509b5d9a766
user <alex@example.com>
2022-08-27 20:48:45 +0300

    tasks 6_1 to 6_2b complete

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,29 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+from curses.ascii import isalnum
+
+
+def check_ip(ip: str):
+   if '.' not in ip: return False
+   ip_oct = ip.split('.')
+   if len(ip_oct) != 4: return False
+   for i in ip_oct:
+      if not i.isdigit(): return False
+   for i in ip_oct:
+      if not (0 <= int(i) <= 255): return False
+   return True
+   
+
+ip = input("Enter IP-adress: ")
+
+if check_ip(ip):
+   ip_oct = ip.split('.')
+   if (1 <= int(ip_oct[0]) <= 223): print("unicast")
+   elif (224 <= int(ip_oct[0]) <= 239): print("multicast")
+   elif ip == "255.255.255.255": print("local broadcast")
+   elif ip == "0.0.0.0": print("unassigned")
+   else:print("unused")
+else:
+   print("Неправильный IP-адрес")
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,36 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+
+def check_ip(ip: str):
+   if '.' not in ip: return False
+   ip_oct = ip.split('.')
+   if len(ip_oct) != 4: return False
+   for i in ip_oct:
+      if not i.isdigit(): return False
+   for i in ip_oct:
+      if not (0 <= int(i) <= 255): return False
+   return True
+   
+
+def get_ip():
+    ip = input("Enter IP-adress: ")
+    if check_ip(ip):
+        return ip
+    else:
+        print("Неправильный IP-адрес")
+        return get_ip()
+
+
+def ip_type(ip):
+    ip_oct = ip.split('.')
+    if (1 <= int(ip_oct[0]) <= 223): return "unicast"
+    elif (224 <= int(ip_oct[0]) <= 239): return "multicast"
+    elif ip == "255.255.255.255": return "local broadcast"
+    elif ip == "0.0.0.0": return "unassigned"
+    else: return "unused"
+
+
+ip = get_ip()
+print(ip_type(ip))
